ebff5718cfa3889a5cf6404b3c7e3ca8e468b98e
Alan de Sant'Anna <108240669+alandesantanna@users.noreply.github.com>
2025-04-13 21:05:54 -0300

    Remover implementação da cifra de César e ajustar impressão de palavras filtradas

--- capitulo5-string/ex04.py
+++ capitulo5-string/ex04.py
@@ -1,24 +1,6 @@
-import string
+palavras = ["algoritmo", "código", "Python", "dados", "função", "laço", "variável", "teste", "cifra", "computação"]
 
-def cifra(astring, chave):
-    uppercase = string.ascii_uppercase
-    lowercase = string.ascii_lowercase
-    encrypt = ''
+palavras_filtradas = [palavra for palavra in palavras if len(palavra) > 4]
 
-    for c in astring:
-        if c in uppercase:
-            new = (uppercase.index(c) + chave) % 26 
-            encrypt += uppercase[new]
-        elif c in lowercase:
-            new = (lowercase.index(c) + chave) % 26
-            encrypt += lowercase[new]
-        else:
-            encrypt += c
-    return encrypt
-
-# Testando a função
-texto = "este é o algoritmo de cifra de César"
-chave = 3
-texto_cifrado = cifra(texto, chave)
-print(f"Texto original: {texto}")
-print(f"Texto cifrado: {texto_cifrado}")
+print(f"Palavras originais: {palavras}")
+print(f"Palavras com mais de 4 caracteres: {palavras_filtradas}")
