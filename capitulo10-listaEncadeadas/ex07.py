7e9a458114b457be3d40567b557dae1efb752cd1
Alan de Sant'Anna <108240669+alandesantanna@users.noreply.github.com>
2025-05-12 19:01:25 -0300

    Adicionar implementação da classe LinkedList com métodos para adicionar e exibir elementos

---
+++ capitulo10-listaEncadeadas/ex07.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+
+linked_list = LinkedList()
+for i in range(1, 101):
+    linked_list.append(i)
+
+linked_list.display()
+
+
